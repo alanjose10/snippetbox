d8eb69e1f0b0e756471d2e8791742d9f8038de49
Alan Jose <alanjose10@gmail.com>
2024-07-18 01:37:13 +0100

    Move routes to different file and cleanup main

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -25,25 +25,9 @@ func main() {
 		logger: logger,
 	}
 
-	// Create a new server mux
-	// and register the home function as handler for /
-	mux := http.NewServeMux()
-
-	fileServe := http.FileServer(http.Dir("./ui/static"))
-
-	// File serve route
-	mux.Handle("GET /static/", http.StripPrefix("/static", fileServe))
-
-	mux.HandleFunc("GET /{$}", app.homeGet)
-
-	mux.HandleFunc("GET /snippet/view/{id}", app.snippetViewGet)
-
-	mux.HandleFunc("GET /snippet/create", app.snippetCreateGet)
-	mux.HandleFunc("POST /snippet/create", app.snippetCreatePost)
-
 	logger.Info("starting server", slog.String("addr", *addr))
 
-	err := http.ListenAndServe(*addr, mux)
+	err := http.ListenAndServe(*addr, app.routes())
 	logger.Error(err.Error())
 	os.Exit(1)
 }
---
+++ cmd/web/routes.go
@@ -0,0 +1,23 @@
+package main
+
+import "net/http"
+
+func (app *application) routes() *http.ServeMux {
+	// Create a new server mux
+	// and register the home function as handler for /
+	mux := http.NewServeMux()
+
+	fileServe := http.FileServer(http.Dir("./ui/static"))
+
+	// File serve route
+	mux.Handle("GET /static/", http.StripPrefix("/static", fileServe))
+
+	mux.HandleFunc("GET /{$}", app.homeGet)
+
+	mux.HandleFunc("GET /snippet/view/{id}", app.snippetViewGet)
+
+	mux.HandleFunc("GET /snippet/create", app.snippetCreateGet)
+	mux.HandleFunc("POST /snippet/create", app.snippetCreatePost)
+
+	return mux
+}
